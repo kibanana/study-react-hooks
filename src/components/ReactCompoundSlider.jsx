973a44ae2e830c23dadd2a67418c7cb076931deb
Kim Yewon <kyw017763@gmail.com>
2021-03-09 11:46:20 +0900

    Add react-compound-slider example

---
+++ src/components/ReactCompoundSlider.jsx
@@ -0,0 +1,349 @@
+import React, { Component, Fragment } from 'react';
+import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
+import './tooltip.scss';
+
+// *******************************************************
+// TOOLTIP RAIL
+// *******************************************************
+const railStyle = {
+  position: 'absolute',
+  width: '100%',
+  transform: 'translate(0%, -50%)',
+  height: 40,
+  cursor: 'pointer',
+  zIndex: 300,
+};
+
+const railCenterStyle = {
+  position: 'absolute',
+  width: '100%',
+  transform: 'translate(0%, -50%)',
+  height: 14,
+  borderRadius: 7,
+  cursor: 'pointer',
+  pointerEvents: 'none',
+  backgroundColor: 'rgb(155,155,155)',
+};
+
+class TooltipRail extends Component {
+  state = {
+    value: null,
+    percent: null,
+  };
+
+  onMouseEnter = () => {
+    document.addEventListener('mousemove', this.onMouseMove);
+  };
+
+  onMouseLeave = () => {
+    this.setState({ value: null, percent: null });
+    document.removeEventListener('mousemove', this.onMouseMove);
+  };
+
+  onMouseMove = (e) => {
+    const { activeHandleID, getEventData } = this.props;
+
+    if (activeHandleID) {
+      this.setState({ value: null, percent: null });
+    } else {
+      this.setState(getEventData(e));
+    }
+  };
+
+  render() {
+    const { value, percent } = this.state;
+    const { activeHandleID, getRailProps } = this.props;
+
+    return (
+      <Fragment>
+        {!activeHandleID && value ? (
+          <div
+            style={{
+              left: `${percent}%`,
+              position: 'absolute',
+              marginLeft: '-11px',
+              marginTop: '-35px',
+            }}
+          >
+            <div className="tooltip">
+              <span className="tooltiptext">Value: {value}</span>
+            </div>
+          </div>
+        ) : null}
+        <div
+          style={railStyle}
+          {...getRailProps({
+            onMouseEnter: this.onMouseEnter,
+            onMouseLeave: this.onMouseLeave,
+          })}
+        />
+        <div style={railCenterStyle} />
+      </Fragment>
+    );
+  }
+}
+
+// *******************************************************
+// SLIDER RAIL (no tooltips)
+// *******************************************************
+const railOuterStyle = {
+  position: 'absolute',
+  transform: 'translate(0%, -50%)',
+  width: '100%',
+  height: 42,
+  borderRadius: 7,
+  cursor: 'pointer',
+};
+
+const railInnerStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: 14,
+  transform: 'translate(0%, -50%)',
+  borderRadius: 7,
+  pointerEvents: 'none',
+  backgroundColor: 'rgb(155,155,155)',
+};
+
+const SliderRail = ({ getRailProps }) => (
+  <Fragment>
+    <div style={railOuterStyle} {...getRailProps()} />
+    <div style={railInnerStyle} />
+  </Fragment>
+);
+
+// *******************************************************
+// HANDLE COMPONENT
+// *******************************************************
+
+class Handle extends Component {
+  state = {
+    mouseOver: false,
+  };
+
+  onMouseEnter = () => {
+    this.setState({ mouseOver: true });
+  };
+
+  onMouseLeave = () => {
+    this.setState({ mouseOver: false });
+  };
+
+  render() {
+    const {
+      domain: [min, max],
+      handle: { id, value, percent },
+      isActive,
+      disabled,
+      getHandleProps,
+    } = this.props;
+    const { mouseOver } = this.state;
+
+    return (
+      <Fragment>
+        {(mouseOver || isActive) && !disabled ? (
+          <div
+            style={{
+              left: `${percent}%`,
+              position: 'absolute',
+              marginLeft: '-11px',
+              marginTop: '-35px',
+            }}
+          >
+            <div className="tooltip">
+              <span className="tooltiptext">Value: {value}</span>
+            </div>
+          </div>
+        ) : null}
+        <div
+          style={{
+            left: `${percent}%`,
+            position: 'absolute',
+            transform: 'translate(-50%, -50%)',
+            WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+            zIndex: 400,
+            width: 26,
+            height: 42,
+            cursor: 'pointer',
+            backgroundColor: 'none',
+          }}
+          {...getHandleProps(id, {
+            onMouseEnter: this.onMouseEnter,
+            onMouseLeave: this.onMouseLeave,
+          })}
+        />
+        <div
+          role="slider"
+          aria-valuemin={min}
+          aria-valuemax={max}
+          aria-valuenow={value}
+          style={{
+            left: `${percent}%`,
+            position: 'absolute',
+            transform: 'translate(-50%, -50%)',
+            WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+            zIndex: 300,
+            width: 24,
+            height: 24,
+            border: 0,
+            borderRadius: '50%',
+            boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.2)',
+            backgroundColor: disabled ? '#666' : '#8b6068',
+          }}
+        />
+      </Fragment>
+    );
+  }
+}
+
+// *******************************************************
+// TRACK COMPONENT
+// *******************************************************
+const Track = ({
+  source,
+  target,
+  getTrackProps,
+  disabled,
+}) => {
+    console.log(target.percent, source.percent, target.percent - source.percent)
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        transform: 'translate(0%, -50%)',
+        height: 14,
+        zIndex: 1,
+        background: '#833ab4', /* fallback for old browsers */
+        background: '-webkit-linear-gradient(to right, #fcb045, #fd1d1d, #833ab4)',  /* Chrome 10-25, Safari 5.1-6 */
+        background: 'linear-gradient(to right, #fcb045, #fd1d1d, #833ab4)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        // backgroundColor: disabled ? '#999' : '#6d4b51',
+        borderRadius: 7,
+        cursor: 'pointer',
+        left: `${source.percent}%`,
+        width: `${target.percent - source.percent}%`,
+      }}
+      {...getTrackProps()}
+    />
+  );
+};
+
+// *******************************************************
+// TICK COMPONENT
+// *******************************************************
+const Tick = ({ tick, count, format = d => d }) => {
+  return (
+    <div>
+      <div
+        style={{
+          position: 'absolute',
+          marginTop: 17,
+          width: 1,
+          height: 5,
+          backgroundColor: 'rgb(200,200,200)',
+          left: `${tick.percent}%`,
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          marginTop: 25,
+          fontSize: 10,
+          textAlign: 'center',
+          marginLeft: `${-(100 / count) / 2}%`,
+          width: `${100 / count}%`,
+          left: `${tick.percent}%`,
+        }}
+      >
+        {format(tick.value)}
+      </div>
+    </div>
+  );
+};
+
+//
+
+const sliderStyle = {
+    position: 'relative',
+    width: '100%',
+  };
+  
+  const domain = [0, 500];
+  const defaultValues = [200];
+  
+  export default class ReactCompoundSlider extends Component {
+    state = {
+      values: defaultValues.slice(),
+      update: defaultValues.slice(),
+    };
+  
+    onUpdate = (update) => {
+      this.setState({ update });
+    };
+  
+    onChange = (values) => {
+      this.setState({ values });
+    };
+  
+    render() {
+      const {
+        state: { values, update },
+      } = this;
+  
+      return (
+        <div style={{ height: 120, width: '100%' }}>
+          <Slider
+            mode={1}
+            step={1}
+            domain={domain}
+            rootStyle={sliderStyle}
+            onUpdate={this.onUpdate}
+            onChange={this.onChange}
+            values={values}
+          >
+            <Rail>
+              {({ getRailProps }) => <SliderRail getRailProps={getRailProps} />}
+            </Rail>
+            <Handles>
+              {({ handles, activeHandleID, getHandleProps }) => (
+                <div className="slider-handles">
+                  {handles.map(handle => (
+                    <Handle
+                      key={handle.id}
+                      handle={handle}
+                      domain={domain}
+                      isActive={handle.id === activeHandleID}
+                      getHandleProps={getHandleProps}
+                    />
+                  ))}
+                </div>
+              )}
+            </Handles>
+            <Tracks left={false} right={false}>
+              {({ tracks, getTrackProps }) => (
+                <div className="slider-tracks">
+                  {tracks.map(({ id, source, target }) => (
+                    <Track
+                      key={id}
+                      source={source}
+                      target={target}
+                      getTrackProps={getTrackProps}
+                    />
+                  ))}
+                </div>
+              )}
+            </Tracks>
+            <Ticks count={5}>
+              {({ ticks }) => (
+                <div className="slider-ticks">
+                  {ticks.map(tick => (
+                    <Tick key={tick.id} tick={tick} count={ticks.length} />
+                  ))}
+                </div>
+              )}
+            </Ticks>
+          </Slider>
+        </div>
+      );
+    }
+}
+  
