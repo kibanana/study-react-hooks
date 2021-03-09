3603fa133cf30f203ac10e534fc93b6743bcd00c
Kim Yewon <kyw017763@gmail.com>
2021-03-10 08:27:02 +0900

    After studying prop types...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactCompoundSlider from './components/ReactCompoundSlider';
-// import ReactGridGallery1 from './components/ReactGridGallery1';
-// import ReactGridGallery2 from './components/ReactGridGallery2';
-// import ReactGridGallery3 from './components/ReactGridGallery3'; // error
-// import ReactGridGallery4 from './components/ReactGridGallery4'; // error
-// import ReactGridGallery5 from './components/ReactGridGallery5'; // error
-// import ReactGridGallery6 from './components/ReactGridGallery6'; // error
+import ReactGridGallery1 from './components/ReactGridGallery1';
+import ReactGridGallery2 from './components/ReactGridGallery2';
+import ReactGridGallery3 from './components/ReactGridGallery3'; // error
+import ReactGridGallery4 from './components/ReactGridGallery4'; // error
+import ReactGridGallery5 from './components/ReactGridGallery5'; // error
+import ReactGridGallery6 from './components/ReactGridGallery6'; // error
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <ReactCompoundSlider />
         </div>
         <div>
-          <ReactGridGallery6 />
+          <ReactGridGallery2 />
         </div>
       </body>
     </div>
