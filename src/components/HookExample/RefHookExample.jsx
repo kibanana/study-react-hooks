d6407cc900a4bdfca91368c32d9d4222810611c3
Kim Yewon <kyw017763@gmail.com>
2021-03-11 10:49:10 +0900

    Fix useRef example

--- src/components/HookExample/RefHookExample.jsx
+++ src/components/HookExample/RefHookExample.jsx
@@ -1,15 +1,27 @@
 import React, { useRef } from 'react';
 
 export default function TextInputWithFocusButton() {
-    const inputEl = useRef(null);
-    const onButtonClick = () => {
+    const inputNameEl = useRef(null);
+    const inputAgeEl = useRef(null);
+
+    const onButtonClick = (e) => {
       // `current` points to the mounted text input element
-      inputEl.current.focus();
+      e.preventDefault()
+      inputNameEl.current.focus();
+      setTimeout(() => { inputAgeEl.current.focus() }, 2000);
     };
     return (
-      <>
-        <input ref={inputEl} type="text" />
+      <form>
+        <label>
+          이름: 
+          <input ref={inputNameEl} type="text" />
+        </label>
+        <hr></hr>
+        <label>
+          나이: 
+          <input ref={inputAgeEl} type="number" />
+        </label>
         <button onClick={onButtonClick}>Focus the input</button>
-      </>
+      </form>
     );
 }
