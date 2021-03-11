d660c5feec07c839614c8651df34d5ebd8e4038a
Kim Yewon <kyw017763@gmail.com>
2021-03-11 14:54:35 +0900

    Fix reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,17 @@
+import * as ACTION from './action'
+
 export default function reducer(state, action) {
     switch (action.type) {
-        case 'add':
-            return {
-                ...state,
-                [action.key]: action.value
-            }
-        case 'delete':
+        case ACTION.CURRENT_USER:
+        case ACTION.HOBBY:
+        case ACTION.HOBBYCOUNT:
+        case ACTION.TODO:
+        case ACTION.TODOCOUNT:
             return {
                 ...state,
                 [action.key]: action.value
             }
         default:
-            throw new Error();
+            return state;
     }
 }
